99f89b6c94e0573aafffaf7ae5e2bf77e77033ab
jerrywanglaoji <1044511102@qq.com>
2018-04-15 02:14:27 +0800

    守护进程test

--- main.cpp
+++ main.cpp
@@ -8,7 +8,10 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <stdio.h>
+#include <string.h>
 
+//令该进程变成守护进程
 void init_daemon()
 {
     int pid;
@@ -17,30 +20,50 @@ void init_daemon()
     //创建子进程，并结束父进程
     //保证进程不是进程组组长进程
     if ((pid = fork()) < 0){
+        printf("fork error\n");
         exit(1);
     }
-    else if (pid != 0)
+    else if (pid > 0)
         exit(0);
 
-    //创建一个会话，会话只包含子进程，企鹅子进程是会话首进程
+    //创建一个会话，会话只包含子进程，其子进程是会话首进程
     setsid();
 
-    //再次创建子进程，并结束父进程
-    //保证守护进程不是会话首进程，这样open的时候不会被分配终端
-    if((pid = fork()) < 0){
-        exit(1);
-    }
-    else if (pid != 0)
-        exit(0);
+    //改变当前工作目录为根目录
+    chdir("/");
 
-    if (chdir("/") < 0){
-        exit(2);
+    //设置文件权限掩码
+    umask(0);
+
+    for(i = 0; i < getdtablesize(); ++i)
+    {
+        //关闭文件权限描述符
+        close(i);
+    }
+}
+
+void func_do()
+{
+    char buf[] = "This is a daemon\n";
+    int fd;
+
+    while(1)
+    {
+        if (fd = open("./daemon.log", O_CREAT|O_WRONLY|O_APPEND, 0600) < 0)
+        {
+            printf("open file error\n");
+            exit(1);
+        }
+        write(fd, buf, strlen(buf) +1);
+        close(fd);
+        sleep(10);
     }
 }
 
 int main()
 {
-    init_daemon();
+    //init_daemon();
+    func_do();
     return 0;
 }
 
